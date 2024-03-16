b37c0a54a812c7583cca304953e2264e866addbb
Jepjep <jeffreyechano@gmail.com>
2024-03-16 15:52:23 +0800

    fix nativewind for web

--- App.js
+++ App.js
@@ -22,12 +22,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Text, View } from 'react-native';
+import { NativeWindStyleSheet } from "nativewind";
+
+NativeWindStyleSheet.setOutput({
+  default: "native",
+});
 
 export default function App() {
   return (
   
     <View className="flex-1 items-center justify-center bg-slate-600">
-      <Text className='text-white'>Open up App.js to start working on your app!</Text>
+      <Text >Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
